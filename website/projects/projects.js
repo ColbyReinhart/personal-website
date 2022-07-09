88e593a11abcf4c363f6ecebbb0c78f3fb3e8e96
Colby Reinhart <colbyreinhart@gmail.com>
2022-07-09 19:13:45 -0400

    Projects page complete.

--- website/projects/projects.js
+++ website/projects/projects.js
@@ -9,3 +9,15 @@ document.querySelector("#symplyfyGalleryClose").addEventListener("click", () => 
 	gallery.style.animationName = "screen-hide";
 	setTimeout(() => {gallery.style.display = "none";}, 950);
 });
+
+document.querySelector("#netpongGalleryButton").addEventListener("click", () => {
+	let gallery = document.querySelector("#netpongGallery");
+	gallery.style.animationName = "screen-appear";
+	gallery.style.display = "block";
+});
+
+document.querySelector("#netpongGalleryClose").addEventListener("click", () => {
+	let gallery = document.querySelector("#netpongGallery");
+	gallery.style.animationName = "screen-hide";
+	setTimeout(() => {gallery.style.display = "none";}, 950);
+});
