90ea2a8a908e18c650b2d7dbe9bb0522eb692ef6
Colby Reinhart <colbyreinhart@gmail.com>
2022-07-19 19:34:17 -0400

    Added more color options.

--- website/lightbox/lightbox.js
+++ website/lightbox/lightbox.js
@@ -10,7 +10,27 @@ document.querySelector("#green").addEventListener("click", () => {
 	req.send("0 255 0");
 });
 
-document.querySelector("#red").addEventListener("click", () => {
+document.querySelector("#blue").addEventListener("click", () => {
 	req.open("PUT", "http://colbyreinhart.com:5000/lightbox");
 	req.send("0 0 255");
+});
+
+document.querySelector("#magenta").addEventListener("click", () => {
+	req.open("PUT", "http://colbyreinhart.com:5000/lightbox");
+	req.send("255 0 255");
+});
+
+document.querySelector("#cyan").addEventListener("click", () => {
+	req.open("PUT", "http://colbyreinhart.com:5000/lightbox");
+	req.send("0 255 255");
+});
+
+document.querySelector("#yellow").addEventListener("click", () => {
+	req.open("PUT", "http://colbyreinhart.com:5000/lightbox");
+	req.send("255 255 0");
+});
+
+document.querySelector("#white").addEventListener("click", () => {
+	req.open("PUT", "http://colbyreinhart.com:5000/lightbox");
+	req.send("255 255 255");
 });
