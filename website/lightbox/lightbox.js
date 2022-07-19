c7ead9285992e0d4c04837cace6025b6aec4014d
Colby Reinhart <colbyreinhart@gmail.com>
2022-07-19 19:31:06 -0400

    Changed request body

--- website/lightbox/lightbox.js
+++ website/lightbox/lightbox.js
@@ -2,15 +2,15 @@ let req = new XMLHttpRequest;
 
 document.querySelector("#red").addEventListener("click", () => {
 	req.open("PUT", "http://colbyreinhart.com:5000/lightbox");
-	req.send("DATA 255 0 0");
+	req.send("255 0 0");
 });
 
 document.querySelector("#green").addEventListener("click", () => {
 	req.open("PUT", "http://colbyreinhart.com:5000/lightbox");
-	req.send("DATA 0 255 0");
+	req.send("0 255 0");
 });
 
 document.querySelector("#red").addEventListener("click", () => {
 	req.open("PUT", "http://colbyreinhart.com:5000/lightbox");
-	req.send("DATA 0 0 255");
+	req.send("0 0 255");
 });
